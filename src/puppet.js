25db2fbd310e0351fee6ba16e70d620c3d86b070
Dipak Dushing <97525751+dipakdushing@users.noreply.github.com>
2022-06-08 19:32:53 +0530

    Update puppet.js
    updated

--- src/puppet.js
+++ src/puppet.js
@@ -3,13 +3,16 @@ const fs=require("fs/promises")
  const start=async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  
   await page.goto("https://www.google.com/search?q=jobs+near+me&ibp=htl;jobs&sa=X&ved=2ahUKEwj8iOnf4ZP4AhW3aGwGHbR6A1QQutcGKAF6BAgEEAY&sxsrf=ALiCzsa3r5oYRNHpRlEZKZw3GTyaEg8kJg:1654344562399#htivrt=jobs&htidocid=m9Ba-Z3N0lMAAAAAAAAAAA%3D%3D&fpstate=tldetail")
   
   let data = await page.evaluate(() => {
-    let results = []
-    let items = document.querySelectorAll('.h2Zk0d')
+   
+    let results = [];
+    let items = document.querySelectorAll('.h2Zk0d');
     
     items.forEach((item) => {
+     
         results.push({
            
             title: item.querySelector('.BjJfJf').textContent,
